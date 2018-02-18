8b72e7ab9473dcb7e28ab19f356f2758e56e17f5
raphaelSeguin <raphaelseguin@gmail.com>
2018-02-18 18:43:32 +0100

    final commit :)

--- js/script.js
+++ js/script.js
@@ -3,27 +3,17 @@
 
 // global variables
 let employeesModels = [];
-const bigIndex = ( () => {
-  let value = 0;
-  return {
-    increment: function(val) {
-      value = (value + val + 12) % 12;
-      return value;
-    },
-    getValue: function() {
-      return value;
-    },
-    setValue: function(val) {
-      value = val;
-    }
-  }
-})();
 
 // elements
 const containerDiv = document.getElementById('container');
 const modalWindow = document.getElementById('modal');
 const fadeOut = document.getElementById('fade-out');
 const filterInput = document.getElementById('filter');
+const modalInfos = document.getElementById('modal-infos');
+// elements on the modal
+const closeModalAnchor = document.getElementById('close-modal');
+const backModalAnchor = document.getElementById('back-modal');
+const forthModalAnchor = document.getElementById('forth-modal');
 
 // utility function
 const capitalizeFirstLetter = word => {
@@ -31,11 +21,16 @@ const capitalizeFirstLetter = word => {
 };
 
 // build the elements and attach event handlers
-const cardFill = (data, index) => {
+const fillCard = (data, index) => {
+  // destructure the variables from the object data
   const {picture, name, email, city} = data;
+  // create a div element
   const card = document.createElement('div');
+  // add class card
   card.classList.add('card');
+  // set the id
   card.id = index;
+  // add content to the div.card
   card.insertAdjacentHTML('afterbegin',
    `<img class="picture" src="${picture}" alt="photo of employee">
     <div class="infos">
@@ -43,47 +38,39 @@ const cardFill = (data, index) => {
       <div class="email">${email}</div>
       <div class="city">${city}</div>
     </div>`);
+  // add an event handler
   card.addEventListener('click', event => {
+    // if the card is not filtered out
     if( !card.classList.contains('filtered-out') ) {
+      // display the modal corresponding to this card
       showModal(card.id);
     }
   });
   return card;
 };
-const modalFill = number => {
+const fillModal = number => {
   const {picture, name, email, cellnumber, address, city, birthdate} = employeesModels[number];
-  modalWindow.className = number;
-  bigIndex.setValue(number);
-  modalWindow.innerHTML =
-   `<a id="close-modal" href="#">X</a>
-    <img class="picture modal" src="${picture}" alt="">
-    <div class="name modal">${name}</div>
-    <div class="email modal">${email}</div>
-    <div class="city modal">${city}</div>
-    <hr></hr>
-    <div class="cellnumber modal">${cellnumber}</div>
-    <div class="address modal">${address}</div>
-    <div class="birthdate modal">${birthdate}</div>
-    <div class="nav-modal">
-      <a id="back-modal" href="#">&lt;&lt;</a>
-      <a id="forth-modal" href="#">&gt;&gt;</a>
-    </div>`;
-  // elements on the modal
-  const closeModalAnchor = document.getElementById('close-modal');
-  const backModalAnchor = document.getElementById('back-modal');
-  const forthModalAnchor = document.getElementById('forth-modal');
-
-  closeModalAnchor.addEventListener('click', () => {
-    modalWindow.style.display = 'none';
-    fadeOut.style.display = 'none';
-  });
-  backModalAnchor.addEventListener('click', () => {
-    modalFill( bigIndex.increment( -1 ) );
-  });
-  forthModalAnchor.addEventListener('click', () => {
-    modalFill( bigIndex.increment( 1 ) );
-  });
+  modalInfos.className = number;
+  modalInfos.innerHTML = `<img class="picture modal" src="${picture}" alt="">
+      <div class="name modal">${name}</div>
+      <div class="email modal">${email}</div>
+      <div class="city modal">${city}</div>
+      <hr></hr>
+      <div class="cellnumber modal">${cellnumber}</div>
+      <div class="address modal">${address}</div>
+      <div class="birthdate modal">${birthdate}</div>`;
 }
+
+const showModal = number => {
+  fillModal(number);
+  modalWindow.classList.add('active');
+  fadeOut.classList.add('active');
+};
+const hideModal = () => {
+  modalWindow.classList.remove('active');
+  fadeOut.classList.remove('active');
+}
+
 // take the API results and spit out only what we need
 const recondition = json => {
   return {
@@ -95,14 +82,9 @@ const recondition = json => {
     city: json.location.city,
     address: json.location.street + ' ' + json.location.postcode,
     country: json.location.state,
-    birthdate: json.dob
+    birthdate: json.dob,
+    filteredOut: false
   }
-};
-
-const showModal = number => {
-  modalFill(number);
-  modalWindow.style.display = 'block';
-  fadeOut.style.display = 'block';
 };
 
 // initialize the page (load the content and fills the page)
@@ -126,7 +108,7 @@ const init = number => {
     employeesModels = json.results.map(recondition);
     return employeesModels;
   })
-  .then( employeesArray => employeesArray.map(cardFill) )
+  .then( employeesArray => employeesArray.map(fillCard) )
   .then( elementArray => elementArray.forEach( (el) => {
     containerDiv.insertAdjacentElement('beforeend', el);
   }))
@@ -155,5 +137,26 @@ filterInput.addEventListener('input', (event) => {
     return object;
   });
 })
+closeModalAnchor.addEventListener('click', () => {
+  hideModal();
+});
+backModalAnchor.addEventListener('click', () => {
+  let cardNumber = parseInt(modalInfos.className);
+  let limit = 12;
+  do {
+    cardNumber = (cardNumber + 11) % 12;
+    limit--;
+  } while( employeesModels[cardNumber].filteredOut === true && limit > 0);
+  showModal( cardNumber );
+});
+forthModalAnchor.addEventListener('click', () => {
+  let cardNumber = parseInt(modalInfos.className);
+  let limit = 12;
+  do {
+    cardNumber = (cardNumber + 13) % 12;
+    limit--;
+  } while( employeesModels[cardNumber].filteredOut === true && limit > 0);
+  showModal( cardNumber );
+});
 
 init(12);
